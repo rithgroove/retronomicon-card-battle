2c723b1ea40a23016dccfd3d78b90a9c7996bf4b
rithgroove <rithgroove@gmail.com>
2025-09-09 08:23:13 +0700

    update action_card

--- include/retronomicon/lib/card-battle/data/action_card.h
+++ include/retronomicon/lib/card-battle/data/action_card.h
@@ -2,24 +2,28 @@
 
 #include "card.h"
 #include <string>
+#include <unordered_map>
 
 namespace retronomicon::lib::cardBattle::data {
 
-class ActionCard : public Card {
-private:
-    int cost;
-    int damage;
+    class ActionCard : public Card {
+    private:
+        std::unordered_map<std::string, int> cost;
+        int damage;
+        std::string effectScript;
 
-public:
-    ActionCard(const std::string& name,
-               const std::string& image,
-               int cost,
-               int damage);
+    public:
+        ActionCard(const std::string& name,
+                   const std::string& image,
+                   const std::unordered_map<std::string, int>& cost,
+                   int damage,
+                   const std::string& effectScript);
 
-    int getCost() const { return cost; }
-    int getDamage() const { return damage; }
+        const std::unordered_map<std::string, int>& getCost() const { return cost; }
+        int getDamage() const { return damage; }
+        const std::string& getEffectScript() const { return effectScript; }
 
-    void play() override;
-};
+        void play() override;
+    };
 
-} // namespace retronomicon::lib::battle
+} // namespace retronomicon::lib::cardBattle::data
--- src/lib/card-battle/data/action_card.cpp
+++ src/lib/card-battle/data/action_card.cpp
@@ -5,15 +5,24 @@ namespace retronomicon::lib::cardBattle::data {
 
 ActionCard::ActionCard(const std::string& name,
                        const std::string& image,
-                       int cost,
-                       int damage)
-    : Card(name, image), cost(cost), damage(damage) {}
+                       const std::unordered_map<std::string, int>& cost,
+                       int damage,
+                       const std::string& effectScript)
+    : Card(name, image),
+      cost(cost),
+      damage(damage),
+      effectScript(effectScript) {}
 
 void ActionCard::play() {
     std::cout << "Playing ActionCard: " << name
-              << " (Cost: " << cost
-              << ", Damage: " << damage << ")\n";
-    // TODO: apply damage, consume resources
+              << " (Damage: " << damage << ")\n";
+    std::cout << "  Costs: ";
+    for (const auto& [type, amount] : cost) {
+        std::cout << amount << " " << type << " ";
+    }
+    std::cout << "\n";
+    std::cout << "  Effect Script: " << effectScript << "\n";
+    // TODO: hook into Lua scripting later
 }
 
-} // namespace retronomicon::lib::battle
+} // namespace retronomicon::lib::cardBattle::data
